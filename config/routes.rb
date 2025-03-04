9b949162365e18b4f01b860c62734f88601621c3
nyastassia <128276412+nyastassia@users.noreply.github.com>
2025-03-05 08:25:43 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'messages/index'
 root to: "messages#index"
 resources :users, only: [:edit, :update]
+resources :rooms, only: [:new, :create]
  get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
