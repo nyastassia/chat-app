7487438303c12fcf2890ae1bb69fa26acc2f2b5e
nyastassia <128276412+nyastassia@users.noreply.github.com>
2025-03-04 09:07:18 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
 root to: "messages#index"
+resources :users, only: [:edit, :update]
  get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
