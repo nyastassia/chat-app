85c3b7dff9660c10f12c016f037fe9678a04b841
nyastassia <128276412+nyastassia@users.noreply.github.com>
2025-03-05 14:42:59 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
-root to: "messages#index"
+root to: "rooms#index"
 resources :users, only: [:edit, :update]
 resources :rooms, only: [:new, :create]
- get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
